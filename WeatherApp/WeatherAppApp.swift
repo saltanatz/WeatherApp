f3732d94d7ac03fdf0e7f40e4bb2c1b047a26ed4
saltanatz <saltanat.zarkhinova@nu.edu.kz>
2024-05-25 14:27:03 +0500

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Saltanat on 25.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
